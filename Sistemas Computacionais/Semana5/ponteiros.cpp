ab8944c38ec1f04852bb015638fa0e2d85179c8c
César L. Guilherme <cesarguilherme@trt15.jus.br>
2022-09-08 09:12:33 -0300

    Ponteiros commit

--- Sistemas Computacionais/Semana5/ponteiros.cpp
+++ Sistemas Computacionais/Semana5/ponteiros.cpp
@@ -4,7 +4,7 @@ int main () {
     int x=8, *p1, *p2;
     p1 = &x;
     p2 = p1;
-    printf ("%d %d", p1, *p1);
-    printf ("%d %d", p2, *p2);
+    printf ("%d %d", p1, *p1, "\n");
+    printf ("%d %d", p2, *p2, "\n");
     return 0;
 }
