8f4fff9e4ecaf688a6af9543cda00ca6b0fa7ad4
SpartanWilhelm <clguilherme3@gmail.com>
2022-10-24 18:56:17 -0300

    Correção 1 String em maiusculo

--- Programação Orientada a Objetos/Semana3/Teste.java
+++ Programação Orientada a Objetos/Semana3/Teste.java
@@ -2,7 +2,7 @@ package Semana3;
 
 public class Teste {
 
-    public static void main (string[] args){
+    public static void main (String[] args){
         CarroCorrida x = new CarroCorrida( );
         x.acelera;
     }   
