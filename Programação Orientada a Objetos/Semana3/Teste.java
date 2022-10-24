af13343473566a481d1f58e2ed5ccfb1e7f652d2
SpartanWilhelm <clguilherme3@gmail.com>
2022-10-24 18:56:59 -0300

    Correção 2

--- Programação Orientada a Objetos/Semana3/Teste.java
+++ Programação Orientada a Objetos/Semana3/Teste.java
@@ -3,7 +3,7 @@ package Semana3;
 public class Teste {
 
     public static void main (String[] args){
-        CarroCorrida x = new CarroCorrida( );
+        CarroCorrida x = new CarroCorrida();
         x.acelera;
     }   
 }
