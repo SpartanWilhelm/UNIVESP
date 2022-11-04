2f6f23481f008bb20ff57179a81548ac9f7a8a7d
SpartanWilhelm <clguilherme3@gmail.com>
2022-11-04 16:34:41 -0300

    Correção

--- Programação Orientada a Objetos/Semana4/MyCollection.java
+++ Programação Orientada a Objetos/Semana4/MyCollection.java
@@ -1,10 +1,6 @@
 package Semana4;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class MyCollection {
     public static void main(String[] args) {
