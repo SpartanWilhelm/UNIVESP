b7776e4e28e08fd10826dd144b375901c8aae2ad
César L. Guilherme <clguilherme3@gmail.com>
2023-04-24 09:10:17 -0300

    Primeiro Projeto

---
+++ Circuitos Digitais/circuito01.vhdl
@@ -0,0 +1,16 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY circuito01 IS
+	PORT ( a, b, c IN STD_LOGIC;
+				y OUT STD_LOGIC);
+END circuito01;
+
+ARCHITECTURE df OF circuito01 IS
+	SIGNAL m : STD_LOGIC;
+
+BEGIN
+	m <= a AND b;
+	y <= m OR c;
+END df;
+
