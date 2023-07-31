4b319a3b9c3452a739b769ed677d944875f7cfd9
César L. Guilherme <clguilherme3@gmail.com>
2023-07-31 09:52:09 -0300

    Código Blink

---
+++ Sistemas Embarcados/CodigoBlink/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH); //turn the LED on
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW); //turn the LED off
+  delay(1000);
+}
