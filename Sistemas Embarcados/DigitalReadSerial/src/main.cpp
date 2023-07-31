99ef02f47da3e85b753892b311a7138c3c3e27a3
César L. Guilherme <clguilherme3@gmail.com>
2023-07-31 10:02:25 -0300

    Digital Serial Read

---
+++ Sistemas Embarcados/DigitalReadSerial/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int pushButton = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pushButton, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int buttonState = digitalRead(pushButton);
+  Serial.println(buttonState);
+  delay(1);
+}
